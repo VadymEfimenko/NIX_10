ad45c1a4fb9998519c1566822334ab5eb25883a2
Vadim Efimenko <vadimpproduction@gmail.com>
2021-10-24 18:46:22 +0300

    module_1.2

--- module_1/src/main/java/ua/com/alevel/L1_T2_Chess.java
+++ module_1/src/main/java/ua/com/alevel/L1_T2_Chess.java
@@ -1,17 +1,48 @@
 package ua.com.alevel;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
+//mismatch ex
 public class L1_T2_Chess {
-    public String horseStepIsPossible(char startPosLetter, int startPosNumber,
-                                      char finPosLetter, int finPosNumber) {
-        Character horse = 9822;
-        Character whiteSquare = 9632;
-        Character blackSquare = 9633;
-        int numberOfStartLetter = (int) Character.toUpperCase(startPosLetter) - 65;
-        int numberOfFinLetter = (int) Character.toUpperCase(finPosLetter) - 65;
+    public void horseStepIsPossible() {
+
+        char startPosLetter;
+        int startPosNumber;
+        char finPosLetter;
+        int finPosNumber;
+
+        //кодировка в юникоде
+        char horse = 9822;
+        char whiteSquare = 9644;
+        char blackSquare = 9645;
+
         char[][] chessBoard = new char[8][8];
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите начальную и желаемую позицию коня: ");
+
+        System.out.println("Начальная позиция, буква A - H: ");
+        startPosLetter = scanner.next().charAt(0);
+
+        System.out.println("Начальная позиция, цифра 1 - 8: ");
+        startPosNumber = scanner.nextInt();
+
+        System.out.println("Желаемая позиция, буква A - H: ");
+        finPosLetter = scanner.next().charAt(0);
+
+        System.out.println("Желаемая позиция, цифра 1 - 8: ");
+        finPosNumber = scanner.nextInt();
+
+        // приведение char к int, выражение букв в цифрах от 1 до 8 для заполнения массива
+        int numberOfStartLetter = (int) Character.toUpperCase(startPosLetter) - 65;
+        int numberOfFinLetter = (int) Character.toUpperCase(finPosLetter) - 65;
+
+        //величина шага хода коня
+        int horseStepLetter = numberOfFinLetter - numberOfStartLetter;
+        int horseStepNumber = finPosNumber - startPosNumber;
+
+        //отрисовка доски в консоли
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
 
@@ -24,19 +55,33 @@ public class L1_T2_Chess {
                 } else if (i % 2 == 1 & j % 2 == 1) {
                     chessBoard[i][j] = whiteSquare;
                 }
-
             }
         }
 
-        chessBoard[startPosNumber - 1][(int) Character.toUpperCase(startPosLetter) - 65] = horse;
+        //Логика
+        try {
+            chessBoard[startPosNumber - 1][numberOfStartLetter] = horse;
+            chessBoard[finPosNumber - 1][numberOfFinLetter] = horse;
+
+            if (Math.abs(horseStepNumber) == 1 && Math.abs(horseStepLetter) == 2 ||
+                    Math.abs(horseStepNumber) == 2 && Math.abs(horseStepLetter) == 1) {
+                System.out.println("Ход возможен");
+            } else {
+                System.out.println("Ход невозможен");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Введите корректные значения! \n" +
+                    "букву латиницей от A до H \n" +
+                    "цифру от 1 до 8");
+        }
 
         for (int i = 0; i < 8; i++) {
             System.out.println(i + 1 + " " + Arrays.toString(chessBoard[i]));
         }
-        System.out.println("  " + " A  " + "B  " + "C  " + "D  " + "E  " + "F  " + "G  " + "H  ");
+        System.out.println("  " + " A  " + " B  " + " C  " + "D  " + " E  " + " F  " + "G  " + " H");
 
 
-        return "yes";
     }
 }
 
--- module_1/src/main/java/ua/com/alevel/Main.java
+++ module_1/src/main/java/ua/com/alevel/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args) {
         //L1_T1_UniqueNumbers l1T1UniqueNumbers = new L1_T1_UniqueNumbers();
         L1_T2_Chess l1_t2_chess = new L1_T2_Chess();
-        l1_t2_chess.horseStepIsPossible('d',4,'c',0);
+        try {
+            l1_t2_chess.horseStepIsPossible();
+        } catch (Exception e) {
+            System.out.println("NO!!!");
+        }
 
     }
 }
+
