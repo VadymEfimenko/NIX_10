5543e656520d7009cbb55fce984f412265f30207
vadim <vadimpproduction@gmail.com>
2023-07-19 12:56:25 +0300

    zcxz

--- hw_1_base_operations/src/main/java/ua/com/alevel/Main.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/Main.java
@@ -7,43 +7,51 @@ public class Main {
 
     //xxx
     public static void main(String[] args) {
-        SumNumbersInString task1 = new SumNumbersInString();
-        CountCharInString task2 = new CountCharInString();
-        EndLessonsTime endLessonsTime = new EndLessonsTime();
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello");
+        System.out.println("hello world cc");
 
-        while (true) {
-            try {
-                System.out.println("enter the number of the task you want to run, from 1 to 3. 0 to exit");
-                int choice = scanner.nextInt();
-                if (choice == 1) {
-                    System.out.println("task 1:");
-                    task1.task1Run();
 
-                } else if (choice == 2) {
-                    System.out.println("task 2:");
-                    task2.task2Run();
 
-                } else if (choice == 3) {
-                    System.out.println("task 3:");
-                    endLessonsTime.task3Run();
 
-                } else if (choice == 0) {
-                    System.out.println("end program");
-                    break;
 
-                } else {
-                    System.out.println("That's not an option!");
-                    continue;
-                }
-            } catch (InputMismatchException input) {
-                System.out.println("That's not an option!");
-                continue;
-            }
-        }
+//        SumNumbersInString task1 = new SumNumbersInString();
+//        CountCharInString task2 = new CountCharInString();
+//        EndLessonsTime endLessonsTime = new EndLessonsTime();
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Hello");
+//
+//        while (true) {
+//            try {
+//                System.out.println("enter the number of the task you want to run, from 1 to 3. 0 to exit");
+//                int choice = scanner.nextInt();
+//                if (choice == 1) {
+//                    System.out.println("task 1:");
+//                    task1.task1Run();
+//
+//                } else if (choice == 2) {
+//                    System.out.println("task 2:");
+//                    task2.task2Run();
+//
+//                } else if (choice == 3) {
+//                    System.out.println("task 3:");
+//                    endLessonsTime.task3Run();
+//
+//                } else if (choice == 0) {
+//                    System.out.println("end program");
+//                    break;
+//
+//                } else {
+//                    System.out.println("That's not an option!");
+//                    continue;
+//                }
+//            } catch (InputMismatchException input) {
+//                System.out.println("That's not an option!");
+//                continue;
+//            }
+//        }
 
 
     }
